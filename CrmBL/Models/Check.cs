6aaf43a41f4441600a02ff3fba216fae9c91ef5e
Сергей Галкин <sergeygalckin2012@yandex.ru>
2021-05-28 20:47:48 +0300

    Добавьте файлы проекта.

---
+++ CrmBL/Models/Check.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBL.Models
+{
+    public class Check
+    {
+        public int CheckId { get; set; }
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+        public int SellerId { get; set; }
+        public virtual Seller Seller { get; set; }
+        public DateTime Created { get; set; }
+        public virtual ICollection<Sale> Sales { get; set; }
+        public decimal Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"№{CheckId} от {Created:dd.MM.yy hh:mm:ss}";
+        }
+    }
+}
---
+++ CrmBL/Models/CrmContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBL.Models
+{
+    public class CrmContext : DbContext
+    {
+        public CrmContext() : base("DBConnection") { }
+        public DbSet<Check> Checks { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Seller> Sellers { get; set; }       
+        
+    }
+}
